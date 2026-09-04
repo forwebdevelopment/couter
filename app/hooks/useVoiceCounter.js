b601de22b011833c68e1d7c287ded1e8b0844413
Krishna <krishna.mohan@sourcemash.com>
2026-09-04 18:01:27 +0530

    updated code

--- app/hooks/useVoiceCounter.js
+++ app/hooks/useVoiceCounter.js
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState } from "react";
 
 export default function useVoiceCounter({
   onCountIncrease,
-  targetWords = ["ram", "राम" , "Radha"],
+  targetWords = ["ram", "राम" , "Radha","राधा"],
   language = "hi-IN",
 }) {
   const recognitionRef = useRef(null);
