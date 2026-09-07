c8e32c285101e4d599edd6b82e4db6001dfc4bc4
Krishna <krishna.mohan@sourcemash.com>
2026-09-07 18:13:18 +0530

    updated code

--- app/(counter)/dashboard/page.js
+++ app/(counter)/dashboard/page.js
@@ -1,5 +1,5 @@
 "use client";
-import Navbar from "../../components/Navbar";
+
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import useVoiceCounter from "../../hooks/useVoiceCounter";
